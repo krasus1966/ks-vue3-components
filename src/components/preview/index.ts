4f9fa9359273a6ba2c71c101b0660b76f976ae32
chenxu <2134123985@qq.com>
2023-09-02 21:15:43 +0800

    html预览（风险），txt预览（格式会错位）

--- src/components/preview/index.ts
+++ src/components/preview/index.ts
@@ -4,6 +4,7 @@ import previewPdfEmbed from './src/preview-pdf-embed.vue'
 import previewPdfJs from './src/preview-pdf-js.vue'
 import previewTxt from './src/preview-txt.vue'
 import previewImage from './src/preview-image.vue'
+import previewHtml from './src/preview-html.vue'
 import { App } from 'vue'
 
 // 让这个组件可以通过use的形式使用
@@ -13,6 +14,7 @@ export default {
     app.component('ks-preview-pdf-embed', previewPdfEmbed)
     app.component('ks-preview-pdf', previewPdfJs)
     app.component('ks-preview-txt', previewTxt)
+    app.component('ks-preview-html', previewHtml)
     app.component('ks-preview-image', previewImage)
     app.component('ks-preview-dialog', previewDialog)
   }
